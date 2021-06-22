97db522955b924d11850576b88c616ad7a63ae95
wangsheng <14213145@qq.com>
2021-06-22 23:35:24 +0800

    1.增加springcloud config 手动配置 2.@RefreshScope // 用来不重启当前应用的情况下，将当前scope域中信息刷新为最新配置 3.# 开启所有的web端点暴漏   management:     endpoints:       web:         exposure:           include: "*" 4.curl -X POST http://localhost:8990/actuator/refresh

--- springcloud_13configclient/src/main/java/com/windsun/controller/TestController.java
+++ springcloud_13configclient/src/main/java/com/windsun/controller/TestController.java
@@ -2,6 +2,7 @@ package com.windsun.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @Slf4j
+@RefreshScope // 用来不重启当前应用的情况下，将当前scope域中信息刷新为最新配置
+//同时curl post 请求http://ip:port/actuator/refresh
 public class TestController {
 
     @Value("${name}")
